f17ba20dff758aad2f11e6c8c52bba485fcdab47
oatsutham <oatsutham@gmail.com>
2026-09-22 18:19:55 +0700

    1.1

---
+++ discovery_piscine/cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Nattachai"
+last_name  = "Sutham"
+print(first_name, last_name)
---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Nattachai"
+last_name  = "Sutham"
+whole_name = first_name + " " + last_name
+print(whole_name)
